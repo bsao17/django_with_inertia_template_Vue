9dcd7003f361d72c9206bc35c8e2b672a38fa14b
Bruno M <declic62gmail.com>
2023-08-16 21:10:09 +0200

    feat: init

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+const { resolve } = require('path');
+
+module.exports = {
+  plugins: [],
+  root: resolve('./assets/src'),
+  base: '/static/',
+  server: {
+    host: 'localhost',
+    port: 3000,
+    open: false,
+    watch: {
+      usePolling: true,
+      disableGlobbing: false,
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+  build: {
+    outDir: resolve('./assets/dist'),
+    assetsDir: '',
+    manifest: true,
+    emptyOutDir: true,
+    target: 'es2015',
+    rollupOptions: {
+      input: {
+        main: resolve('./assets/src/js/main.js'),
+      },
+      output: {
+        chunkFileNames: undefined,
+      },
+    },
+  },
+};
